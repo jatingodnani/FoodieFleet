b0c3d2ed9cc9615b01b7e7ce5bb1fc5eb95b0334
jatin <jatingodnani@gmail.com>
2023-09-11 14:22:14 +0530

    ,dl,l

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
 import Image from 'next/image'
 import Header from './components/header'
 import Cards from './components/cards'
+import { PrismaClient } from '@prisma/client'
+import handler from './api/seed';
+const prisma=new PrismaClient();
 
-export default function Home() {
+const fetchresto=async()=>{
+ let res=await prisma.restaurant.findMany()
+  return res
+}
+export default async function Home() {
+  const restaurant=await fetchresto();
+console.log(restaurant?restaurant:null);
   return (
     <main className="bg-gray-100 min-h-screen w-screen overflow-x-hidden">
   <main className="max-w-screen-2xl m-auto bg-white">
